bf5726ad8b9989c3415f30c74529b8b60c7d2bab
meetshahcode <72691054+meetshahcode@users.noreply.github.com>
2022-06-07 22:55:52 +0530

    admin panel completed

--- smartnotes/home/url.py
+++ smartnotes/home/url.py
@@ -1,7 +1,7 @@
-from django.urls import URLPattern, path
+from django.urls import path
 
 from . import views
 
-URLPattern = [
+urlpatterns = [
     path("home",views.home,{}),
 ]
--- smartnotes/home/views.py
+++ smartnotes/home/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from datetime import datetime
+
+
 def home(request):
     return render(request,"home/welcome.html",{"today":datetime.today()})
