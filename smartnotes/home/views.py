dcaf03de392b5396385d476544db468b5dd9d902
meetshahcode <72691054+meetshahcode@users.noreply.github.com>
2022-06-07 23:03:26 +0530

    authorized needed
    @login_required
    def authorized(request):
        return render(request, "home/authorized.html",{})
    
    from django.contrib.auth.decorators import login_required

--- smartnotes/home/views.py
+++ smartnotes/home/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from datetime import datetime
-
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request,"home/welcome.html",{"today":datetime.today()})
+
+@login_required
+def authorized(request):
+    return render(request, "home/authorized.html",{})
