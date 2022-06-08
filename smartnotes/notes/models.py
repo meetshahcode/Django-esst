f35bbdafadb36881a514726953ba0d16a0a56947
meetshahcode <72691054+meetshahcode@users.noreply.github.com>
2022-06-08 12:04:04 +0530

    model created and updated to database

---
+++ smartnotes/notes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Note(models.Model):
+    Title = models.CharField(max_length=200)
+    notes = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    
