a5e04021a5f9f17a04b1d95df2d83485872f80e1
meetshahcode <72691054+meetshahcode@users.noreply.github.com>
2022-06-11 15:23:30 +0530

    User added
     user = models.ForeignKey(to=User,on_delete=models.CASCADE,related_name="note")

--- smartnotes/home/url.py
+++ smartnotes/home/url.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home",views.Homeview.as_view(),name = "home"),
+    path("",views.Homeview.as_view(),name = "home"),
     path("authorized",views.authorizedview.as_view(),name = "authorized")
 ]
--- smartnotes/notes/models.py
+++ smartnotes/notes/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
 
 class Note(models.Model):
     Title = models.CharField(max_length=200)
     notes = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(to=User,on_delete=models.CASCADE,related_name="note")
     def __str__(self):
         return self.Title
